fc6072e4da699bfccbecd54219717f94b49ad60a
Frezan <49377394+Frezan@users.noreply.github.com>
2022-05-12 13:39:22 +0200

    Add the token verif on request

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,18 +3,21 @@
 namespace App\DataFixtures;
 
 use Faker\Factory;
-use App\Entity\Department;
+use App\Entity\User;
+use App\Entity\Profil;
 use App\Entity\Commune;
+use App\Entity\Department;
 use App\Repository\RegionRepository;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
-    function __construct(RegionRepository $regionRepository)
+    function __construct(RegionRepository $regionRepository, UserPasswordHasherInterface $passwordHasher )
     {
         $this->regionRepository = $regionRepository;
+        $this->userPasswordHasherInterface = $passwordHasher;
     }
 
     public function load(ObjectManager $manager): void
@@ -43,6 +46,26 @@ class AppFixtures extends Fixture
 
 
         $manager->flush();
-    }
+
+        $profils = ['ADMIN', 'FORMATEUR', 'APPRENANT', 'CM'];
+      
+        foreach ($profils as $key => $libelle) {
+            $profil = new Profil();
+            $profil->setLibelle($libelle);
+            $manager->persist($profil);
+            $manager->flush();
+
+            for ($i=0; $i <=3 ; $i++) { 
+                $user = new User();
+                $user->setProfil($profil)
+                     ->setLogin(strtolower($libelle).$i)
+                     ->setNomComplet($faker->name());
+            $hasher = $this->userPasswordHasherInterface->hashPassword($user, 'passer123');
+            $user->setPassword($hasher);
+            $manager->persist($user);
+            }
+            $manager->flush();
+        }
+    }   
 }
 
